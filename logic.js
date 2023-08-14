6b296569ee495795bcefc87d9d34a93c2638ce94
Jawad Shaikh <J.shk1980@gmail.com>
2023-08-14 13:35:31 -0400

    made the board work and moving onto the calculator project

--- logic.js
+++ logic.js
@@ -1,13 +1,20 @@
 const container = document.querySelector('#board')
 
-let row = 200
-let col = 200
-for (let i = 0; i < (row*col); i++){
-    let div = document.createElement("div")
-    div.style.backgroundColor = 'white'
-    div.style.width = '1px'
-    div.style.height = '1px'
-    container.style.gridTemplateColumns = `repeat(${row}, 1fr)`
-    container.style.gridTemplateRows = `repeat(${col}, 1fr)`
-    container.appendChild(div).classList.add('box')
+
+
+function makeBoard(row,col){
+    for (let i = 0; i < (row*col); i++){
+        let div = document.createElement("div")
+        div.style.backgroundColor = 'white'
+        div.style.width = '50px'
+        div.style.height = '50px'
+        div.addEventListener("mouseover",()=>{
+            div.style.backgroundColor = 'black'
+        })
+        container.style.gridTemplateColumns = `repeat(${row}, 0fr)`
+        container.style.gridTemplateRows = `repeat(${col}, 0fr)`
+        container.appendChild(div).classList.add('box')
 }
+}
+let sizeOfBoard = prompt("How Big Do You Want The Board ? MAX 300","200")
+makeBoard(sizeOfBoard,sizeOfBoard)
